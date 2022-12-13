4fa97324baa4071a691eaa8d363de2e495a19b5f
Redu96 <redu1996@gmail.com>
2022-12-13 22:20:06 +0100

    Añadidos los modelos Entrenadores y Pagos, ademas de unos ligeros cambios

--- src/main/java/com/proyecto/app/controllers/PaymentController.java
+++ src/main/java/com/proyecto/app/controllers/PaymentController.java
@@ -6,10 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.proyecto.app.Repositories.ComentarioRepository;
-import com.proyecto.app.Repositories.RutinaRepository;
-import com.proyecto.app.Repositories.UsuarioRepository;
 import com.proyecto.app.services.WarmUpServices;
 
 @Controller
@@ -17,15 +13,6 @@ public class PaymentController {
 
 	@Autowired
 	private WarmUpServices wUpService;
-
-	@Autowired
-	private RutinaRepository rutinasRepo;
-
-	@Autowired
-	private UsuarioRepository usuarioRepo;
-
-	@Autowired
-	private ComentarioRepository comentRepo;
 
 	private static final String REDIRECT = "redirect:";
 
